f59289e03a0155d4226de1f85c616c97444b6457
Min <63823931+MuteMini@users.noreply.github.com>
2024-01-07 22:55:55 -0500

    header comments, initial setup for implementation

--- src/chip8.cpp
+++ src/chip8.cpp
@@ -1,2 +1,16 @@
+/*
+    Author: Min Kang
+    Creation Date: January 7th, 2024
+
+    Defines the behaviours of the Chip8 system. Works as a high level abstraction.
+    Instr. behavior were written with the help of: https://github.com/mattmikolay/chip-8/wiki/CHIP%E2%80%908-Technical-Reference#graphics
+*/
+
 #include "chip8.hpp"
 
+Chip8::Chip8() {};
+
+Chip8::~Chip8() {
+    delete[] memory;
+    delete[] display;
+};
--- src/chip8.hpp
+++ src/chip8.hpp
@@ -1,9 +1,17 @@
+/*
+    Author: Min Kang
+    Creation Date: January 7th, 2024
+
+    Declares the behaviours of the Chip8 system.
+    Structure/variable types were made with the help of this blog: https://austinmorlan.com/posts/chip8_emulator/ 
+*/
+
 #include <cstdint>
 
 class Chip8 {
     private:
         uint8_t reg[16]{};
-        uint8_t memory[4096]{};
+        uint8_t *memory{ new uint8_t[4096]{} };
         uint16_t index_reg{};
         uint16_t pc{};
 
@@ -13,12 +21,15 @@ class Chip8 {
         uint8_t delay{};
         uint8_t sound{};
 
-        uint8_t display[64*32]{};
+        uint8_t *display{ new uint8_t[64*32]{} };
 
     public:
         Chip8();
+        ~Chip8();
 
-        bool loadProgram();
+        bool loadProgram(uint8_t data);
 
         bool tick();
+
+        
 };
