75afc885c335608fdd18812ffdc0c56549f3fdf8
Min <63823931+MuteMini@users.noreply.github.com>
2024-01-13 17:48:24 -0500

    fixed build folder deletion in batch

--- script/rebuild.bat
+++ script/rebuild.bat
@@ -1,4 +1,4 @@
 echo off
-rm -r build
+rmdir /s /q build
 cmake -B build -S . -DVCPKG_TARGET_TRIPLET="x64-mingw-dynamic" -DCMAKE_TOOLCHAIN_FILE="vcpkg/scripts/buildsystems/vcpkg.cmake" --preset=default
 ninja -C build
