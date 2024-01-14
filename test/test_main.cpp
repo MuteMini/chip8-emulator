7d1f6001e4fbf050150bf117415adc00391d947b
Min <63823931+MuteMini@users.noreply.github.com>
2024-01-14 12:35:24 -0500

    fixed testing file

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -4,12 +4,16 @@
 
     Entry point for unit testing. Utilizes Doctest that is ran by custom script in run.sh.
 */
-#define DOCTEST_
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <doctest/doctest.h>
 
-#include "../src/chip8.hpp"
+#include "chip8.hpp"
 
-TEST_CASE("testing the factorial function") {
-    CHECK(true);
-    CHECK(false);
+TEST_CASE("Chip8 functionality") {
+    Chip8 chip8{};
+
+    SUBCASE("loadProgram") {
+        CHECK(chip8.loadProgram("\\test\\_data\\IBMLogo.ch8"));
+        CHECK(!chip8.loadProgram("\\test\\_data\\nofile.ch8"));
+    }
 }
