afd0e601dd167c3bce4ca0a709540f3baf753793
Min <63823931+MuteMini@users.noreply.github.com>
2024-01-23 23:08:33 -0500

    refactor: targets into preset

--- script/rebuild.bat
+++ script/rebuild.bat
@@ -2,5 +2,5 @@
 IF EXIST env.txt FOR /F "delims=" %%A IN (env.txt) DO SET "%%A"
 
 rmdir /s /q build
-"%CMAKE_PATH%\cmake.exe" -B build -S . -DVCPKG_TARGET_TRIPLET="x64-mingw-dynamic" -DCMAKE_TOOLCHAIN_FILE="vcpkg/scripts/buildsystems/vcpkg.cmake" --preset=default
+"%CMAKE_PATH%\cmake.exe" -B build -S . --preset=default
 ninja -C build
