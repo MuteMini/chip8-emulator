b32c167c129d9a163268508666e961b1e9161b81
Min <63823931+MuteMini@users.noreply.github.com>
2024-01-29 21:56:08 -0500

    setup keyboard

---
+++ src/keyboard/keyboard.hpp
@@ -0,0 +1,31 @@
+/*
+    Author: Min Kang
+    Creation Date: January 29th, 2022
+
+    Declares wrapper for SDL keyboard input event handling.
+    Passes events onto Chip8 through the bus.
+*/
+
+#ifndef KEYBOARD_H
+#define KEYBOARD_H
+
+#include <SDL_keyboard.h>
+
+#include <cstdint>
+
+#include "header.hpp"
+#include "bus.hpp"
+
+class Keyboard : public Component {
+    private:
+        uint8_t key;
+
+    public:
+        Keyboard();
+
+        uint8_t getKey();
+
+        void pollKey();
+};
+
+#endif
--- src/main.hpp
+++ src/main.hpp
@@ -1,9 +1,17 @@
+/*
+    Author: Min Kang
+    Creation Date: January 17th, 2024
+
+    Declares design of bus for main program.
+*/
+
 #ifndef MAIN_H
 #define MAIN_H
 
 #include <memory>
 
 #include "chip8.hpp"
+#include "keyboard.hpp"
 #include "display.hpp"
 #include "bus.hpp"
 
@@ -11,6 +19,7 @@ class MainBus : public Bus
 {
     private:
         Chip8 *cpu{};
+        Keyboard *keyboard{};
         Display *display{};
 
     public:
